7a29fd9547eeb14a10a8b841ed4943203be085f0
tania <taniabellis__@hotmail.com>
2025-12-18 23:01:07 -0500

    fixed img movin, updated card msg

--- lisa.js
+++ lisa.js
@@ -23,7 +23,7 @@ function revealLisa()
     document.getElementById("minilisa").style.display="block";
 }
 var i = 0;
-var txt = "lisa: i hope you have a jolly and restful holiday season! i'm grateful to share this team with such a lovely human being; thank you for being exactly who you are (and for stanning kendrick lamar) ♡";
+var txt = "lisa: i hope you have a jolly and restful holiday season! i'm grateful to share a team with such a wondrous human being; thank you for being exactly who you are (and for stanning kendrick lamar) ♡♡♡";
 var speed = 50;
 
 function typeWriter() {
